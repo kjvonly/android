03ef9a9ccc8f0a4186e353db46fb40723c11844b
man4christ <man4christ.65qa8@simplelogin.com>
2023-09-22 23:24:14 -0400

    Replaces rendered boolean with LaunchEffect using book ViewModel variable as a key.

--- bible-ui/src/main/java/kjvonly/bible/ui/Chapter.kt
+++ bible-ui/src/main/java/kjvonly/bible/ui/Chapter.kt
@@ -15,7 +15,9 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kjvonly.bible.data.models.Book
 import kjvonly.bible.data.models.Chapter
+
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -29,7 +31,7 @@ fun ChapterView(viewModel: ChapterViewModel = hiltViewModel()) {
             state = swipeableState,
             anchors = mapOf(0f to 0, 1f to 1),
             thresholds = { _, _ -> FractionalThreshold(0.3f) },
-            orientation = Orientation.Horizontal
+            orientation = Orientation.Horizontal,
         )
     ) {
         if (swipeableState.isAnimationRunning) {
@@ -45,12 +47,12 @@ fun ChapterView(viewModel: ChapterViewModel = hiltViewModel()) {
         }
         Column(modifier = Modifier.verticalScroll(scrollState)) {
             Verses(
+                book = viewModel.book.value,
                 chapter = viewModel.chapter.value,
                 fontSize = viewModel.fontSize.value,
                 verseRowRenderedHeightMap = viewModel.verseRowRenderedHeightMap,
                 verseToScrollTo = viewModel.book.value.verse,
-                st = scrollState,
-                rendered = viewModel.rendered
+                st = scrollState
             )
         }
     }
@@ -58,65 +60,67 @@ fun ChapterView(viewModel: ChapterViewModel = hiltViewModel()) {
 
 @Composable
 fun Verses(
+    book: Book,
     chapter: Chapter,
     fontSize: Int,
     verseRowRenderedHeightMap: HashMap<Int, Float>,
     verseToScrollTo: Int,
     st: ScrollState,
-    rendered: MutableState<Boolean>,
 ) {
-    val sorted = chapter.verseMap.keys.toList().sorted()
-    for (i in sorted) {
-        Row(modifier = Modifier
-            .padding(start = 10.dp, end = 10.dp, top = 5.dp, bottom = 5.dp)
-            .onGloballyPositioned { coordinates ->
-                verseRowRenderedHeightMap[i] = coordinates.size.height.toFloat()
-            }
-        ) {
-            val v = chapter.verseMap[i]
-            Text(
-                "$i $v",
-                fontSize = fontSize.sp,
-                color = MaterialTheme.colors.onSurface,
+    if (chapter.number != -1) {
+        val sorted = chapter.verseMap.keys.toList().sorted()
+        for (i in sorted) {
+            Row(modifier = Modifier
+                .padding(start = 10.dp, end = 10.dp, top = 5.dp, bottom = 5.dp)
+                .onGloballyPositioned { coordinates ->
+                    verseRowRenderedHeightMap[i] = coordinates.size.height.toFloat()
+                }
+            ) {
+                val v = chapter.verseMap[i]
+                Text(
+                    "$i $v",
+                    fontSize = fontSize.sp,
+                    color = MaterialTheme.colors.onSurface,
                 )
+            }
         }
+
+        ScrollStateComp(book, verseToScrollTo, st, verseRowRenderedHeightMap)
     }
 
-    ScrollStateComp(rendered, verseToScrollTo, st, verseRowRenderedHeightMap)
 }
 
 @Composable
 private fun ScrollStateComp(
-    rendered: MutableState<Boolean>,
+    book: Book,
     verseToScrollTo: Int,
     st: ScrollState,
     verseRowRenderedHeightMap: HashMap<Int, Float>
 ) {
     val composableScope = rememberCoroutineScope()
-    if (!rendered.value) {
-        Row(Modifier
-            .onGloballyPositioned {
-                if (verseToScrollTo == 1) {
-                    composableScope.launch {
-                        st.animateScrollTo(0)
-                        rendered.value = true
-                    }
-                } else {
-                    var totalPixelSizeOfVerses = 0f
-                    val versesToInclude = verseToScrollTo - 1
-                    (1..versesToInclude).forEach {
-                        var v = verseRowRenderedHeightMap[it]
-                        if (v == null) {
-                            v = 0f
-                        }
-                        totalPixelSizeOfVerses += v
-                    }
-                    composableScope.launch {
-                        st.animateScrollTo(totalPixelSizeOfVerses.toInt())
-                        rendered.value = true
-                    }
+
+    LaunchedEffect(book) {
+        if (verseToScrollTo == 1) {
+            composableScope.launch {
+                st.animateScrollTo(0)
+            }
+        } else {
+            var totalPixelSizeOfVerses = 0f
+            val versesToInclude = verseToScrollTo - 1
+            (1..versesToInclude).forEach {
+                var v = verseRowRenderedHeightMap[it]
+                if (v == null) {
+                    v = 0f
+                }
+                totalPixelSizeOfVerses += v
+            }
+            if (totalPixelSizeOfVerses.toInt() != 0) {
+                composableScope.launch {
+                    st.animateScrollTo(totalPixelSizeOfVerses.toInt())
                 }
             }
-        ) {}
+        }
     }
+
 }
+
